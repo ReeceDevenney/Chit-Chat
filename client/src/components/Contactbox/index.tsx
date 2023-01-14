ce8a312b09311655f1bef42fb67ad381f77f1e2c
Reece Devenney <reecedevenney@gmail.com>
2023-01-14 16:00:02 -0500

    added scrollbar and hover styling to message boxes

--- client/src/components/Contactbox/index.tsx
+++ client/src/components/Contactbox/index.tsx
@@ -7,62 +7,62 @@ function Contactbox() {
 
     const messages: Messages[] =[
         {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
+            name: 'Reece Devenney1',
+            message: "This is a test message for testing another thing"
         },
         {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
-        },
-        {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
-        },
-        {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
-        },
-        {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
-        },
-        {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
-        },
-        {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
-        },
-        {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
-        },
-        {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
-        },
-        {
-            name: 'Reece Devenney',
-            message: "This is a test message for testing another ting"
+            name: 'Reece Devenney2',
+            message: "This is a test message for testing another thing"
         },
         {
             name: 'Reece Devenney3',
-            message: "This is a test message for testing another ting"
+            message: "This is a test message for testing another thing"
         },
         {
-            name: 'Reece Devenney2',
-            message: "This is a test message for testing another ting"
+            name: 'Reece Devenney4',
+            message: "This is a test message for testing another thing"
         },
         {
-            name: 'Reece Devenney2',
-            message: "This is a test message for testing another ting"
+            name: 'Reece Devenney5',
+            message: "This is a test message for testing another thing"
+        },
+        {
+            name: 'Reece Devenney6',
+            message: "This is a test message for testing another thing"
+        },
+        {
+            name: 'Reece Devenney7',
+            message: "This is a test message for testing another thing"
+        },
+        {
+            name: 'Reece Devenney8',
+            message: "This is a test message for testing another thing"
+        },
+        {
+            name: 'Reece Devenney9',
+            message: "This is a test message for testing another thing"
+        },
+        {
+            name: 'Reece Devenney10',
+            message: "This is a test message for testing another thing"
+        },
+        {
+            name: 'Reece Devenney11',
+            message: "This is a test message for testing another thing"
+        },
+        {
+            name: 'Reece Devenney12',
+            message: "This is a test message for testing another thing"
+        },
+        {
+            name: 'Reece Devenney13',
+            message: "This is a test message for testing another thing"
         },
     ] 
     return(
         <div>
         {messages.map((message) => 
-        <div className="text-center outline outline-1 h-20 flex">
+        <div className="text-center hover:bg-slate-100 hover:cursor-pointer h-20 flex">
           <div className="bg-gray-300 rounded-full h-14 w-14 my-auto ml-4 items-center flex shrink-0">
             <div className="mx-auto font-bold text-3xl">{message.name[0]}</div>
           </div>
